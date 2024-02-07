51215715ecc48ab7766c4599497f2ca85f6bc59d
ShreyasSharma0 <shreyas.college.study@gmail.com>
2024-02-07 11:33:55 +0530

    Win Counter Added
    Added a win counter, optimized JS for the same

--- tacToeScript.js
+++ tacToeScript.js
@@ -6,6 +6,15 @@ const currentTurn = () => `it is ${currentPlayer}s turn`;
 let gameState = ["", "", "", "", "", "", "", "", ""];
 const reset = document.getElementById("resetButton");
 
+let xWins = parseInt(localStorage.getItem('xWins') || 0);
+let oWins = parseInt(localStorage.getItem('oWins') || 0);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('xCounter').textContent = `X: ${xWins}`
+    document.getElementById('oCounter').textContent = `O: ${oWins}`
+});
+
+
 reset.addEventListener('click', resetGame);
 
 const cells = document.querySelectorAll('.cell');
@@ -16,8 +25,7 @@ cells.forEach((cell, index) => {
 const winConditions = [
     [1, 2, 3], // top row
     [4, 5, 6], // mid row
-    [7, 8, 9], // bottom row
-    [1, 4, 7], // first col
+    [7, 8, 9], // bottom row    [1, 4, 7], // first col
     [2, 5, 8], // mid col 
     [3, 6, 9], // last col
     [3, 5, 7], // diagonal 1
@@ -41,9 +49,8 @@ function handleCellClick(cellIndex) {
     console.log(`Clicked cell ${activeCell}, currentPlayer: ${currentPlayer}`);
     update();
 
-    checkGameWin();
-    announcements();
-
+    const gameWin = checkGameWin();
+    announcements(gameWin);
 
     setTimeout(playerTurn, 2);
 }
@@ -58,30 +65,32 @@ function update() {
 
 
 function checkGameWin() {
-    let gameWin = false;
 
     for (let i = 0; i < winConditions.length; i++) {
         const [x, y, z] = winConditions[i];
         if (gameState[x - 1] && gameState[x - 1] === gameState[y - 1] && gameState[y - 1] === gameState[z - 1]) {
-            gameWin = true;
+
             console.log(`Winning condition met: ${x}, ${y}, ${z}`);
-            break;
+            return true;
         }
     }
-    return gameWin;
+    return false;
 }
 
-function announcements() {
-    if (checkGameWin()) {
+
+function announcements(gameWin) {
+    if (gameWin) {
         update();
+        updateWinCounters();
         setTimeout(announceWin, 1);
         gameOngoing = false;
-    }
-    let gameDrawn = !gameState.includes("");
-    if (gameDrawn) {
-        update();
-        setTimeout(announceDraw, 1)
-        gameOngoing = false;
+    } else {
+        let gameDrawn = !gameState.includes("");
+        if (gameDrawn) {
+            update();
+            setTimeout(announceDraw, 1)
+            gameOngoing = false;
+        }
     }
 }
 
@@ -91,12 +100,11 @@ function announceWin() {
 
 function announceDraw() {
     alert("The game has ended in a draw, better luck next time!");
-
 }
 
 function resetGame() {
     gameOngoing = true;
-    gameState = ["", "", "", "", "", "", "", "", "", ""];
+    gameState = ["", "", "", "", "", "", "", "", ""];
     alert("The Page will reload to start a new game");
     setTimeout(reloadPage, 10);
 }
@@ -104,6 +112,29 @@ function resetGame() {
 function reloadPage() {
     location.reload();
 }
+
+
+function updateXcounter() {
+    xWins++;
+    document.getElementById('xCounter').textContent = `X : ${xWins}`;
+    localStorage.setItem('xWins', xWins.toString());
+}
+
+function updateOCounter() {
+    oWins++;
+    document.getElementById('oCounter').textContent = `O: ${oWins}`;
+    localStorage.setItem('oWins', oWins.toString());
+
+}
+
+function updateWinCounters() {
+    if (currentPlayer === 'X') {
+        updateXcounter();
+    } else if (currentPlayer === 'O') {
+        updateOCounter();
+    }
+}
+
 
 
 
